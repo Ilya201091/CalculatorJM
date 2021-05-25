3cc0eaa27c6bf1d4613bb6553863734db5518bae
Ilya201091 <ZiK201091@ya.ru>
2021-05-25 16:54:51 +0700

    changes in class Main

--- src/CalculatorJM/Main.java
+++ src/CalculatorJM/Main.java
@@ -1,11 +1,16 @@
 package CalculatorJM;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите формулу через пробел. Пример '2 + 2'");
+        Scanner scr = new Scanner(System.in);
 
-        Formula formula = new Formula("V * X");
-        LimitCalculator limitCalculator = new LimitCalculator("20 + 20");
+        String formula = scr.nextLine();
+        Formula formula1 = new Formula(formula);
+        LimitCalculator limitCalculator = new LimitCalculator(formula);
     }
-
+10
 
 }
